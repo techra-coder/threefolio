c628633e5404b063dace2e03acbd2f8893787346
techra-coder <122772457+techra-coder@users.noreply.github.com>
2023-03-08 11:43:06 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from 'react-router-dom'
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
+
+
+const App = () => {
+  
+  return (
+      <div>
+        <h1>3Dev Portfolio</h1>
+      </div>
+  )
+}
+
+export default App
